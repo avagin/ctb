b03f24e68465ab00496de6d1a5a1890647e3f7e0
Andrei Vagin <avagin@openvz.org>
2016-08-26 01:03:46 -0700

    mount: wait all mount processes

--- mounts/run.sh
+++ mounts/run.sh
@@ -1,4 +1,17 @@
+set -m -e
+
+iteration=30
+if [ -n "$1" ]; then
+	iteration=$1
+fi
+
 mount -t tmpfs xxx /mnt
 mount --make-shared /mnt
-for i in `seq 30`; do mount --bind /mnt `mktemp -d /mnt/test.XXXXXX` & done
-umount -l /mnt
+
+for i in `seq $iteration`; do
+	mount --bind /mnt `mktemp -d /mnt/test.XXXXXX` &
+done
+wait
+
+cat /proc/self/mountinfo | grep /mnt | wc -l
+time umount -l /mnt

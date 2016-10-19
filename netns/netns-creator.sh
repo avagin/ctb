85c1d7a19bf55bffb3d62d4d5550daca87196181
Andrei Vagin <avagin@openvz.org>
2016-10-19 16:26:15 -0700

    netns: improve the perf test

--- netns/netns-creator.sh
+++ netns/netns-creator.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+free -m
 mkdir -p netns
 mount -t tmpfs netns netns
 for i in `seq $1`; do
 	touch netns/$i
 	unshare -n -- mount --bind /proc/self/ns/net netns/$i || break
 done
+free -m
 umount -l netns
